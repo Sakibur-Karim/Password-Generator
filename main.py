0368bc78b642bf9fa90538f83508d01c36207401
Sakibur-Karim <sakiburkarim@gmail.com>
2023-07-15 13:00:19 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+s = input('Enter password length: ')
+s = int(s)
+while s<8:
+    s = input('Please re-enter with length of at least 8 characters: ')
+    s = int(s)
+
+chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
+caps = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+num = '1234567890'
+sym = '~!@#$%^&*()_+{}|:"<>?`-=[]\;,./'
+
+password = ''
+
+password += random.choice(num)
+password += random.choice(caps)
+password += random.choice(sym)
+
+for i in range(s-3):
+    password += random.choice(chars)
+
+print(''.join(random.sample(password,len(password))))
